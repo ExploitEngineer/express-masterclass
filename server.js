2a8d5cfd6acc030acca65653574ac71b4d36d8ee
ExploitEngineer <revilwhoami0098@gmail.com>
2025-03-17 23:05:20 +0500

    Add simple routing & .env file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+// setup static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+
+let posts = [
+    { id: "1", title: "Post One" },
+    { id: "2", title: "Post Two" },
+    { id: "3", title: "Post Three" },
+];
+
+app.get('/api/posts', (req, res) => {
+    res.json(posts);
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));

5d623a8192f84fb863e6a1e8b627e8fd432f8fe7
ExploitEngineer <revilwhoami0098@gmail.com>
2025-03-18 02:02:52 +0500

    Add Routes

--- express_v5/server.js
+++ express_v5/server.js
@@ -3,6 +3,16 @@ import express from 'express';
 const app = express();
 
 
+// Creating Routes
+app.get('/', (req, res) => {
+    res.send('Welcome to coders bank!');
+});
+
+app.get('/hello', (req, res) => {
+    res.send('hello!')
+});
+
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}....`))

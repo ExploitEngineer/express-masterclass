a0f1d689550b2c01732190dbcaaed9bcbc758d9d
ExploitEngineer <revilwhoami0098@gmail.com>
2025-03-18 00:00:08 +0500

    Add updated posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -46,4 +46,31 @@ router.post('/', (req, res) => {
     res.status(201).json(posts);
 });
 
+// Update Post
+router.put('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id == id);
+
+    if (!post) {
+        return res.status(404).json({ message: `A post with the id of ${id} was not found` });
+    }
+
+    post.title = req.body.title;
+    res.status(200).json(posts);
+});
+
+
+// Delete Post
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id == id);
+
+    if (!post) {
+        return res.status(404).json({ message: `A post with the id of ${id} was not found` });
+    }
+
+    posts = posts.filter((post) => post.id != id);
+    res.status(200).json(posts);
+});
+
 export default router;

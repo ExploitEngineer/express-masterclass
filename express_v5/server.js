65a6bcab5c6155282edc4030a76448f3a3cb2821
ExploitEngineer <revilwhoami0098@gmail.com>
2025-03-18 02:42:53 +0500

    Add middlewares

--- express_v5/server.js
+++ express_v5/server.js
@@ -2,16 +2,38 @@ import express from 'express';
 
 const app = express();
 
+// Global middleware
+app.use(express.json());
+
+// Custom middleware
+const reqLogger = (req, res, next) => {
+    console.log(`${req.method}: ${req.url} ${new Date().toISOString()}`);
+    next();
+};
+app.use(reqLogger);
+
+// middleware for specific routes - then after url add the function as a second parameter in route
+
 
 // Creating Routes
-app.get('/', (req, res) => {
-    res.send('Welcome to coders bank!');
+app.get('/health', (req, res) => {
+    // console.log(req.query);
+    // res.status(400).json({ message: "All good!" });
+    res.send('Hey! I am healthy!');
 });
 
-app.get('/hello', (req, res) => {
-    res.send('hello!')
+app.post('/api/users', reqLogger, (req, res) => {
+    throw new Error('something went wrong!');
+    res.json({});
 });
 
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+
+    res.status(500).json({ message: "Something broke!" });
+});
 
 const PORT = process.env.PORT || 4000;
 

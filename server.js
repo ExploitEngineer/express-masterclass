981de2f6f7904b20efb1b16d158e32fc173f4b8c
ExploitEngineer <revilwhoami0098@gmail.com>
2025-03-17 23:42:22 +0500

    Add updated server.js

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-const express = require('express');
-const path = require('path');
+import express from 'express'
+import path from 'path';
+import posts from './routes/posts.js';
 const port = process.env.PORT || 8000;
 
 const app = express();
 
-// setup static folder
-// app.use(express.static(path.join(__dirname, 'public')));
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-let posts = [
-    { id: "1", title: "Post One" },
-    { id: "2", title: "Post Two" },
-    { id: "3", title: "Post Three" },
-];
-
-app.get('/api/posts', (req, res) => {
-    res.json(posts);
-});
+// Routes
+app.use('/api/posts', posts);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));

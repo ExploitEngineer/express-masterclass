417c9ad7c170bdf2f35a0911dce61a11cfbbfd8f
ExploitEngineer <revilwhoami0098@gmail.com>
2025-05-24 23:10:35 +0500

    feat: add cookies

--- new_express/src/index.mjs
+++ new_express/src/index.mjs
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser("secret"));
 app.use(routes);
 
 const PORT = process.env.PORT || 4000;
@@ -13,5 +15,6 @@ app.listen(PORT, function () {
 });
 
 app.get("/", (request, response) => {
-  response.status(200).send({ msg: "Hello" });
+  response.cookie("hello", "world", { maxAge: 30000, signed: true });
+  response.status(201).send({ msg: "Hello" });
 });
